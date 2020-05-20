488ab57b4360520facde7ed535d8626270f9fbb1
malhotra-parul <parulm.business@gmail.com>
2020-05-20 13:00:32 +0530

    added firebase config file

---
+++ src/components/firebase.js
@@ -0,0 +1,19 @@
+  import firebase from "firebase/app";
+  import "firebase/auth";
+  import "firebase/database";
+  import "firebase/storage";
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyChckhW8GzilQxPZ2ybKsSIklmbyNAiA3A",
+    authDomain: "react-slack-clone-ce751.firebaseapp.com",
+    databaseURL: "https://react-slack-clone-ce751.firebaseio.com",
+    projectId: "react-slack-clone-ce751",
+    storageBucket: "react-slack-clone-ce751.appspot.com",
+    messagingSenderId: "7348118071",
+    appId: "1:7348118071:web:433c88a6279cfcb47c95b2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
