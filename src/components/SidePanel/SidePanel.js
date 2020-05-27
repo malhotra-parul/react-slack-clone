68314c8552a1f8aba9c7441b01e91db95c18bd99
malhotra-parul <parulm.business@gmail.com>
2020-05-27 20:30:14 +0530

    created channels header

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserPanel from "./UserPanel";
+import Channels from "./Channels";
 import {Menu} from "semantic-ui-react";
 
 class SidePanel extends Component {
@@ -15,6 +16,7 @@ class SidePanel extends Component {
             style={{background: "#4c3c4c", fontSize: "1.2rem"}}
             >
             <UserPanel currentUser={currentUser}/>
+            <Channels />
             </Menu>
          );
     }
