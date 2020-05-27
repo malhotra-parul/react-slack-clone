a19a0559ac6880813c7e14e59222dd7a070ca592
malhotra-parul <parulm.business@gmail.com>
2020-05-27 14:57:09 +0530

    started creating components inside App component

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SidePanel extends Component {
+
+    render() { 
+        return ( 
+            <div>
+            Side Panel
+            </div>
+         );
+    }
+}
+ 
+export default SidePanel;
