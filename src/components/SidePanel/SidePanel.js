f8292ec935bb00b002fdf8565b5ac389d1c11aec
malhotra-parul <parulm.business@gmail.com>
2020-05-27 18:03:30 +0530

    built sidepanel - header and dropdown

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import UserPanel from "./UserPanel";
+import {Menu} from "semantic-ui-react";
 
 class SidePanel extends Component {
 
     render() { 
         return ( 
-            <div>
-            Side Panel
-            </div>
+            <Menu
+            size="large"
+            inverted
+            fixed="left"
+            vertical
+            style={{background: "#4c3c4c", fontSize: "1.2rem"}}
+            >
+            <UserPanel />
+            </Menu>
          );
     }
 }
