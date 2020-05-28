8a5d112ecb41f513a092e06dcf03aa075c8808bc
malhotra-parul <parulm.business@gmail.com>
2020-05-28 15:52:19 +0530

    scaffolding of messages component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Message, Segment } from 'semantic-ui-react';
 
 class Messages extends Component {
 
     render() { 
         return ( 
-            <div>
-            Messages
-            </div>
+           <Fragment>
+               <MessagesHeader />
+               
+               <Segment>
+                    <Comment.Group>
+                        {/* Meesages will come here! */}
+                    </Comment.Group>
+               </Segment>
+
+               <MessageForm />
+           </Fragment>
          );
     }
 }
