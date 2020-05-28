73f3e37dee3abe86e9df627e11a000584fab7816
malhotra-parul <parulm.business@gmail.com>
2020-05-28 15:46:24 +0530

    implemneted color panel

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
 
 class ColorPanel extends Component {
 
     render() { 
         return ( 
-            <div>
-            Color Panel
-            </div>
+            <Sidebar
+            vertical 
+            as={Menu}
+            icon="labeled"
+            inverted
+            visible
+            width="very thin"
+            style={{background: "#ecb241"}}
+            >
+                <Divider />
+                <Button icon="add" style={{background: "#4b9abe"}} size="small" inverted/>
+            </Sidebar>
          );
     }
 }
