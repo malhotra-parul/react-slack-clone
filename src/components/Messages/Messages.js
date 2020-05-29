d812fa5b21f0bd0cc06fb411d92eb1235f4bb7a6
malhotra-parul <parulm.business@gmail.com>
2020-05-29 10:45:24 +0530

    display messages inside channels

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -3,29 +3,66 @@ import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
 import { Segment, Comment } from 'semantic-ui-react';
 import firebase from "../firebase";
+import Message from "./Message";
 
 class Messages extends Component {
 
     state = {
         messagesRef: firebase.database().ref("messages"),
         currentChannel: this.props.currentChannel,
-        user: this.props.currentUser
+        user: this.props.currentUser,
+        messages: [],
+        messagesLoading: true
     }
 
+    componentDidMount(){
+        const {user, currentChannel} = this.state;
+
+        if(user && currentChannel){
+        this.addListeners(currentChannel.id);
+        }
+    }
+
+    addListeners = (channelId)=>{
+        this.addMessageListener(channelId);
+    }
+
+    addMessageListener = (channelId) => {
+        let loadedMessages = [];
+        this.state.messagesRef.child(channelId).on("child_added", snap =>{
+            loadedMessages.push(snap.val());
+            this.setState({
+                messages: loadedMessages,
+                messagesLoading: false
+            })
+        })
+    }
+
+    displayMessages = (messages) => (
+     
+            messages.length>0 && messages.map(message =>(
+                <Message
+                key={message.timestamp}
+                message={message}
+                user={this.state.user} />
+            ))
+    )
+
     render() { 
+        const {currentChannel, messagesRef, messages, user} = this.state;
         return ( 
            <Fragment>
                <MessagesHeader />
                
                <Segment>
                     <Comment.Group className="messages">
-                        {/* Meesages will come here! */}
+                      {this.displayMessages(messages)}
                     </Comment.Group>
                </Segment>
 
-               <MessageForm currentChannel={this.state.currentChannel} 
-                            messagesRef={this.state.messagesRef}
-                            user={this.state.user} />
+               <MessageForm currentChannel={currentChannel} 
+                            messagesRef={messagesRef}
+                            user={user} />
            </Fragment>
          );
     }
