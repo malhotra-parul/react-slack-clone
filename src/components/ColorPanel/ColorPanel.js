c66c840fe8162b5eda52dd7032e191d9ad68a67b
malhotra-parul <parulm.business@gmail.com>
2020-06-02 10:54:12 +0530

    created a modal to change app color

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
+import { Sidebar, Menu, Divider, Button, Modal } from "semantic-ui-react";
 
 class ColorPanel extends Component {
 
+    state={
+        modal: false
+    }
+
+    openModal = ()=> this.setState({modal: true});
+    closeModal = ()=> this.setState({modal: false});
+
     render() { 
+        const {modal} = this.state;
         return ( 
             <Sidebar
             vertical 
@@ -15,7 +23,18 @@ class ColorPanel extends Component {
             style={{background: "#ecb241"}}
             >
                 <Divider />
-                <Button icon="add" style={{background: "#4b9abe"}} size="small" inverted/>
+                <Button icon="add" 
+                        style={{background: "#4b9abe"}} 
+                        size="small" 
+                        inverted
+                        onClick={this.openModal}/>
+                <Modal 
+                basic
+                open={modal}
+                onClose={this.closeModal}
+                >
+
+                </Modal>
             </Sidebar>
          );
     }
